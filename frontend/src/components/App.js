900e328c29c78f85dc62479240ec5b8857156bba
Josip Pušić <pusicjosip1000@gmail.com>
2020-07-27 19:07:03 +0200

    add react router to package.json

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,25 +1,11 @@
-// import '../css/style.css'
+import '../css/style.css'
+import React from 'react'
 
-import React, {Component} from "react"
-import GrandParent from "./GrandParent"
 
-class App extends Component {
-    state = { count: 0 }
-    
-    increment = () => this.setState(prevState => ({count: prevState.count + 1}))
-    
-    render() {
-        console.log("[GP] [P] [C] [GC] APP just rendered")
-        return (
-            <div>
-                <button onClick={this.increment}>+1</button>
-                <h2>{this.state.count}</h2>
-                <p>I'm the App component</p>
-                <GrandParent count={this.state.count} />
-                <GrandParent count={this.state.count} />
-            </div>
-        )    
-    }
+function App() {
+    return (
+        <h1>disi</h1>
+    )
 }
 
 export default App
